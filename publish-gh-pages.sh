c78c95dd0850198e1be0a5ac58c5d3975364f657
Oisín Mac Fhearaí <denpashogai@gmail.com>
2018-07-22 20:46:46 +0100

    Add script for publishing to gh-pages (or wrecking everything)

---
+++ publish-gh-pages.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+echo "Now in: $(pwd)"
+
+wat
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Pruning old version..."
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public..."
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files (again?)"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish-gh-pages.sh)" && cd .. && git push origin gh-pages
