c95fe2e9db97d68a665461ca754c7ec9edff886a
Oisín Mac Fhearaí <denpashogai@gmail.com>
2018-07-22 20:48:03 +0100

    Copy CNAME while publishing

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -32,4 +32,5 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
+cp CNAME public
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish-gh-pages.sh)" && cd .. && git push origin gh-pages
